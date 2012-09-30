4474935c96e755f906a4dfdf55bd3fc4fa32e38d
jocelynk <jocelynk@andrew.cmu.edu>
2012-09-30 02:50:16 -0400

    added 5 scaffolds:dept, events, programs, reg, teams

---
+++ app/models/program.rb
@@ -0,0 +1,3 @@
+class Program < ActiveRecord::Base
+  attr_accessible :active, :active, :department_id, :description, :max_capacity, :max_grade, :min_grade, :name
+end
---
+++ app/models/registration.rb
@@ -0,0 +1,3 @@
+class Registration < ActiveRecord::Base
+  attr_accessible :program_id, :student_id, :team_id
+end
