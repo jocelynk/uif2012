e2e3cf5471824b0e73f5951b239411675c485f51
aakintil <aakintil@andrew.cmu.edu>
2012-10-05 14:06:23 -0400

    sometings up with FactoryGirl.build

--- test/unit/group_test.rb
+++ test/unit/group_test.rb
@@ -7,5 +7,7 @@ class GroupTest < ActiveSupport::TestCase
   
   #Test Validations
   should validate_presence_of(:name)
+  should validate_numericality_of(:max_capacity, :greater_than => 1
+  # validates_numericality_of :max_capacity, :greater_than => 1, :message => "must be greater than 1", :allow_nil => true
   
 end
