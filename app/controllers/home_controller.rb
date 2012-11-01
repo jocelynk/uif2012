632476eb1dcbd7512756a7c8511d83fcc9c12105
jocelynk <jocelynk@andrew.cmu.edu>
2012-10-31 20:21:41 -0400

    adding barcode code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   def index
    @events = Event.current
+   @barcode = params[:barcode]
+     puts "------init------"
+     @print = "your barcode is: #{@barcode}";  
+     puts @print
+
+
+     if (!@barcode.nil?)
+       flash[:notice] = "Barcode was successfully scanned"
+     else
+       flash[:notice] = "There was something wrong with the scanning!" 
+     end
   end
 
   def about
