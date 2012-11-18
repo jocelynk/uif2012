93839b6904727621c2593ba04ef1d922e565f578
Seth Vargo <sethvargo@gmail.com>
2012-11-18 15:15:39 -0500

    fixed javascript

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,48 +16,18 @@ class HomeController < ApplicationController
    @students = Student.search(@query)
    @total_hits = @students.size
   end
-  #Need to Fix this later for more than one section!!!!
-  #Need to check later, right now getting param from URL, good way to do this?
-  #store it in session variables instead
+
   def checkin
-    if !params[:event_id].nil?
-      session[:event_id] = params[:event_id]
+    @event_details = Event.joins('INNER JOIN section_events AS se ON se.event_id = events.id INNER JOIN sections s ON s.id = se.section_id').where('events.id = ?', params[:event_id]).select('s.name AS section, events.date')
+
+    if params[:barcode] && params[:event_id]
+      @student = Student.find_by_barcode_number(params[:barcode])
+
+      if @student && @student.attendances.create(event_id: params[:event_id])
+        render :json => { message: "#{@student.proper_name} was successfully scanned!" }
+      else
+        render :json => { error: 'There was something wrong with the scan!' }
+      end
     end
-    
-    #@event_id = (params[:e_id].nil?)? params[:event_id] : params[:e_id]
-    @event_id = session[:event_id]
-    @event_details = Event.joins('INNER JOIN section_events AS se ON se.event_id = events.id INNER JOIN sections s ON s.id = se.section_id').where('events.id = ?', @event_id).select('s.name AS section, events.date')
-    @barcode = params[:barcode]
-    puts "------init------"
-    @print = "your barcode is: #{@barcode}";  
-    puts @print
-
-    if (!@barcode.nil?)
-       @student_id = Student.where('students.barcode_number = ?', @barcode).select('students.id')
-       @attendance = Attendance.new({:student_id => @student_id.first.id, :event_id =>  @event_id})
-       @attendance.save
-       flash[:notice] = "asdfasdf"
-       render :update do |page|
-        flash[:notice] = "Barcode was successfully scanned"
-         puts "+++++++++++++++++++++++++"
-        puts flash[:notice]
-        page.reload_flash
-       end
-
-       #respond_to do |format|
-       # format.html #{ redirect_to checkin_url, notice: 'asdfasdfadsf' }
-       # format.json {render json: @event_details, status: "test", location: "testtest"}
-       # format.js {render json: @event_details, status: "test", location: "testtest"}
-      # end
-       
-     else
-       flash[:notice] = "qwerqewr"
-       render :checkin do |page|
-        flash[:notice] = "There was something wrong with the scanning!"
-        puts "============================"
-        puts flash[:notice]
-        page.reload_flash
-       end
-     end
   end
 end
