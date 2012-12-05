84ee2e590aa1fd69c0cfe91f53b9718cbc3652e9
jocelynk <jocelynk@andrew.cmu.edu>
2012-12-05 03:10:07 -0500

    fixed photo method

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -57,9 +57,18 @@ class MobileController < ApplicationController
   def getPhoto
     puts "====================="
     puts params
-    respond_to do |format|
+    @student = Student.find_by_id(2)
+    if @student.update_attributes(:photo => params[:file])
+      respond_to do |format|
 
-        format.json { render :json=>{:message=>"There was an error with the barcodes or event."}, :callback => params[:callback] }
+          format.json { render :json=>{:message=>"Picture was successfully uploaded"}, :callback => params[:callback] }
+      end
+    else
+     respond_to do |format|
+
+        format.json { render :json=>{:error=>"There was an error with the upload"}, :callback => params[:callback] }
+    end
+    end
   end
    
 end
