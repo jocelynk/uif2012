446e581ee2e55a6b5a4559b4b6de37a3c8147c5f
profh <profh@cmu.edu>
2012-12-13 10:46:35 -0500

    Fixing shared paths issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,8 @@ set :branch, 'master'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log', 'public/uploads']
+# set :shared_paths, ['config/database.yml', 'log', 'public/uploads']
+set :shared_paths, ['log', 'public/uploads']
 
 # Optional settings:
 set :user, 'deploy'   # Username in the server to SSH to.
