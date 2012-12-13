2332df82c155aa87b63f6f48949b186eeca804cd
profh <profh@cmu.edu>
2012-12-13 08:47:35 -0500

    Removing sudo from unicorn_uif2012 restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,8 @@ namespace :unicorn do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, roles: :app, except: { no_release: true } do
-      run "#{try_sudo} /etc/init.d/unicorn_#{application} #{command}"
+      # run "#{try_sudo} /etc/init.d/unicorn_#{application} #{command}"
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
 
