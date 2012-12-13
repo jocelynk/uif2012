720a3c569319b7b5589b095a675d6b9a942885df
profh <profh@cmu.edu>
2012-12-13 09:35:58 -0500

    Modifying unicorn_init.sh to explicitly use root

--- config/deploy/unicorn_init.sh
+++ config/deploy/unicorn_init.sh
@@ -5,26 +5,34 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deploy/apps/uif2012/current
 PID=/tmp/unicorn.uif2012.pid
-CMD="(cd $APP_ROOT && bundle exec unicorn -D -c $APP_ROOT/config/deploy/unicorn.rb -E production)"
-action="$1"
+# CMD="(cd $APP_ROOT && bundle exec unicorn -D -c $APP_ROOT/config/deploy/unicorn.rb -E production)"
+# action="$1"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/deploy/unicorn.rb -E production"
+AS_USER=root
 set -u
 
-old_pid="$PID.oldbin"
-
-cd $APP_ROOT || exit 1
+OLD_PIN="$PID.oldbin"
 
 sig () {
   test -s "$PID" && kill -$1 `cat $PID`
 }
 
 oldsig () {
-  test -s $old_pid && kill -$1 `cat $old_pid`
+  test -s $OLD_PIN && kill -$1 `cat $OLD_PIN`
 }
 
-case $action in
+run () {
+  if [ "$(id -un)" = "$AS_USER" ]; then
+    eval $1
+  else
+    su -c "$1" - $AS_USER
+  fi
+}
+
+case "$1" in
 start)
   sig 0 && echo >&2 "Already running" && exit 0
-  su -c "$CMD" - deploy
+  run "$CMD"
   ;;
 stop)
   sig QUIT && exit 0
@@ -37,27 +45,27 @@ force-stop)
 restart|reload)
   sig HUP && echo reloaded OK && exit 0
   echo >&2 "Couldn't reload, starting '$CMD' instead"
-  su -c "$CMD" - deploy
+  run "$CMD"
   ;;
 upgrade)
   if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
   then
     n=$TIMEOUT
-    while test -s $old_pid && test $n -ge 0
+    while test -s $OLD_PIN && test $n -ge 0
     do
       printf '.' && sleep 1 && n=$(( $n - 1 ))
     done
     echo
 
-    if test $n -lt 0 && test -s $old_pid
+    if test $n -lt 0 && test -s $OLD_PIN
     then
-      echo >&2 "$old_pid still exists after $TIMEOUT seconds"
+      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
       exit 1
     fi
     exit 0
   fi
   echo >&2 "Couldn't upgrade, starting '$CMD' instead"
-  su -c "$CMD" - deploy
+  run "$CMD"
   ;;
 reopen-logs)
   sig USR1
@@ -67,3 +75,62 @@ reopen-logs)
   exit 1
   ;;
 esac
+
+# old_pid="$PID.oldbin"
+# 
+# cd $APP_ROOT || exit 1
+# 
+# sig () {
+#   test -s "$PID" && kill -$1 `cat $PID`
+# }
+# 
+# oldsig () {
+#   test -s $old_pid && kill -$1 `cat $old_pid`
+# }
+# 
+# case $action in
+# start)
+#   sig 0 && echo >&2 "Already running" && exit 0
+#   su -c "$CMD" - deploy
+#   ;;
+# stop)
+#   sig QUIT && exit 0
+#   echo >&2 "Not running"
+#   ;;
+# force-stop)
+#   sig TERM && exit 0
+#   echo >&2 "Not running"
+#   ;;
+# restart|reload)
+#   sig HUP && echo reloaded OK && exit 0
+#   echo >&2 "Couldn't reload, starting '$CMD' instead"
+#   su -c "$CMD" - deploy
+#   ;;
+# upgrade)
+#   if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
+#   then
+#     n=$TIMEOUT
+#     while test -s $old_pid && test $n -ge 0
+#     do
+#       printf '.' && sleep 1 && n=$(( $n - 1 ))
+#     done
+#     echo
+# 
+#     if test $n -lt 0 && test -s $old_pid
+#     then
+#       echo >&2 "$old_pid still exists after $TIMEOUT seconds"
+#       exit 1
+#     fi
+#     exit 0
+#   fi
+#   echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+#   su -c "$CMD" - deploy
+#   ;;
+# reopen-logs)
+#   sig USR1
+#   ;;
+# *)
+#   echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
+#   exit 1
+#   ;;
+# esac
