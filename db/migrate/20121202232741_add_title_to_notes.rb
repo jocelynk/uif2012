789c2f1e6615ed6dbf0156a514995367822e8272
profh <profh@cmu.edu>
2012-12-17 10:43:05 -0500

    Clearing out old migrations completely

--- db/migrate/20121202232741_add_title_to_notes.rb
+++
@@ -1,5 +0,0 @@
-class AddTitleToNotes < ActiveRecord::Migration
-  def change
-    add_column :notes, :title, :string
-  end
-end
--- db/migrate/20121205221245_add_avatar_columns_to_students.rb
+++
@@ -1,9 +0,0 @@
-class AddAvatarColumnsToStudents < ActiveRecord::Migration
-   def self.up
-    add_attachment :students, :avatar
-  end
-
-  def self.down
-    remove_attachment :students, :avatar
-  end
-end
